ad8b47e75370013f3791226f740435d01458a3d8
Joshua Smith <josmith@icct.com>
2014-05-13 20:51:36 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,4 @@
-##install necessary packages
+##install/upload packages
 install.packages("reshape2") ##install reshape package
 library(reshape2) ##upload reshape package
 
@@ -22,44 +22,48 @@ colnames(data) <- features_names ##adds column names to text data
 extract_columns <- c(grep("mean", features_names), grep("std", features_names)) ##creates a vector of mean and std columns
 extract_data <- data[,extract_columns]
 
+##clean feature names
+colnames(extract_data) <- sub("\\()", "", colnames(extract_data)) ##removes R forbidden ()
+colnames(extract_data) <- sub("-std", "StandardDeviation", colnames(extract_data)) ##expands standard deviation, removes '-' 
+colnames(extract_data) <- sub("-", "", colnames(extract_data)) ##removes '-' 
+colnames(extract_data) <- tolower(colnames(extract_data)) ##transforms all column names to lowercase
+
 ##label activity
 activity_labels_number <- rbind(y_test, y_train) ##combines activity labels (MATCHES ORDER OF merge()!)
 activity_labels_number <- activity_labels_number[,1] ##transforms activity_labels_number in vector
 activity_labels_list <- list() ##creates empty list for activity labels
 for (n in activity_labels_number) { ##converts numeric activity list to activity label
 	if (n == 1) {
-		x <- "Walking"
+		x <- "walking"
 		}
 	if (n == 2) {
-		x <- "WalkingUpstairs"
+		x <- "walkingupstairs"
 		}
 	if (n == 3) {
-		x <- "WalkingDownstairs"
+		x <- "walkingdownstairs"
 		}
 	if (n == 4) {
-		x <- "Sitting"
+		x <- "sitting"
 		}
 	if (n == 5) {
-		x <- "Standing"
+		x <- "standing"
 		}
 	if (n == 6) {
-		x <- "Laying"
+		x <- "laying"
 		}
 	activity_labels_list <- rbind(activity_labels_list, x)
 	}
 
-##adds activity labels to extracted data
+##add activity labels to extracted data
 extract_data$activity <- activity_labels_list
 	
 ##add subject labels
 subject_list <- rbind(subject_test, subject_train) ##combines test and train subject list (MATCHES ORDER of merged data)
-extract_data$subject <- subject_list
+extract_data$subjectnumber <- subject_list
 
-##averages for activity and subject
+##average by activity and subject
 data_melt <- melt(extract_data, id = c("activity", "subject"), measure.vars=c(1:79)) ##melts data by activity and subject
 tidy_data <- dcast(data_melt, subject + activity ~ variable, mean) ##creates new data set with averages per subject per activity
 
-##write to txt file
+##write to .txt file
 write.table(tidy_data, "tidy_data.txt", sep = "\t")
-
-
